4fe5dd9faa64d0b296d802c976eb09bd1fa691e5
rtclark <rtcalrkv@icloud.com>
2024-01-23 21:02:04 -0600

    made an array list and some stuff.

--- src/main/java/dev/corestone/mapprotect/regions/RegionBounds.java
+++ src/main/java/dev/corestone/mapprotect/regions/RegionBounds.java
@@ -1,4 +1,10 @@
 package dev.corestone.mapprotect.regions;
 
+import org.bukkit.Location;
+import org.bukkit.util.BoundingBox;
+
 public class RegionBounds {
+    private BoundingBox box;
+    private Location location1;
+    private Location location2;
 }
--- src/main/java/dev/corestone/mapprotect/regions/RegionBox.java
+++ src/main/java/dev/corestone/mapprotect/regions/RegionBox.java
@@ -1,4 +1,9 @@
 package dev.corestone.mapprotect.regions;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class RegionBox {
+
+    private ArrayList<UUID> players = new ArrayList<>();
 }
