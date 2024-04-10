c971cf608e16a7d1efa6304c9572c05adfae1f09
rtclark <rtclarkv@icloud.com>
2024-04-10 10:28:28 -0500

    sus

--- src/main/java/dev/corestone/mapprotect/utilities/BroadcastSound.java
+++ src/main/java/dev/corestone/mapprotect/utilities/BroadcastSound.java
@@ -6,5 +6,6 @@ import org.bukkit.Sound;
 public class BroadcastSound {
     public static void playSound(Location location, Sound sound, int volume, int pitch){
         location.getWorld().playSound(location, sound, volume, pitch);
+        System.out.println("amogus");
     }
 }
